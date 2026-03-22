281f38a4ac02f022078b7d9154da5dec505f37a0
6em-dev <rimit58872@gmail.com>
2026-03-22 17:24:05 +0100

    more

--- cmds/learnmore.js
+++ cmds/learnmore.js
@@ -2,7 +2,7 @@ module.exports = {
 type: "interactionCreate",
 code: `
 $if[$customID==learnmore;
-You pushed the button
+To automatically ban/kick bots, since most popular discord bots don't have that feature, *yet*. We had to make our own for our server.
 $ephemeral
 ]
 `}
