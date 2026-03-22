0c5234cd40cd65441436d4977f739b28a46c74fe
6em-dev <rimit58872@gmail.com>
2026-03-22 20:23:44 +0100

    please work!!!!

--- cmds/forums.js
+++ cmds/forums.js
@@ -5,6 +5,7 @@ $onlyIf[$djsEval[ctx.states?.channel?.new?.parent?.isThreadOnly()]==true;]
 $log[New forum post created: "$channelName" by $threadOwnerID in <#$channelParentID>]
 $sendMessage[$channelID;Adding staff members for visibility...]
 $addThreadMember[$guildID;$channelID;$guildOwnerID[$guildID]]
+$fetchMembers[$guildID]
 $arrayLoad[admins;,;$roleMembers[$guildID;$findRole[$guildID;Admin];,]]
 $arrayForEach[admins;uid;$addThreadMember[$guildID;$channelID;$env[uid]]]
 `
