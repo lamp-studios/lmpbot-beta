f49a3ab3d9730e2475bb938889574bf5adfee932
6em-dev <rimit58872@gmail.com>
2026-03-22 15:44:32 +0100

    add this

--- cmds/verify.js
+++ cmds/verify.js
@@ -2,5 +2,5 @@ module.exports = {
     type: "messageCreate",
     name: "verify",
     code: `
-$if[$getMemberVar[is_verified;$authorID;$guildID;false]==false;$setMemberVar[is_verified;true;$authorID;$guildID] $c[$memberAddRoles[$guildID;$authorID;$getGuildVar[verification_role;$guildID;$findRole[$guildID;verified]]]] Verified successfully!;You are already verified!]`
+$if[$getMemberVar[is_verified;$authorID;$guildID;false]==false;$setMemberVar[is_verified;true;$authorID;$guildID] $memberAddRoles[$guildID;$authorID;$getGuildVar[verification_role;$guildID;$findRole[$guildID;verified]]] Verified successfully!;You are already verified!]`
 };
