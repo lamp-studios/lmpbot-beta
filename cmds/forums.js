e72cb14c65c85ecab71de0dcbd3255bfc35c0338
6em-dev <rimit58872@gmail.com>
2026-03-22 20:10:50 +0100

    new changes

--- cmds/forums.js
+++ cmds/forums.js
@@ -3,5 +3,9 @@ module.exports = {
     code: `
 $onlyIf[$djsEval[ctx.states?.channel?.new?.parent?.isThreadOnly()]==true;]
 $log[New forum post created: "$channelName" by $threadOwnerID in <#$channelParentID>]
+$sendMessage[$channelID;Adding staff members for visibility...]
+$addThreadMember[$guildID;$channelID;$guildOwnerID[$guildID]]
+$arrayLoad[admins;,;$roleMembers[$guildID;$findRole[$guildID;Admin];,]]
+$arrayForEach[admins;uid;$addThreadMember[$guildID;$channelID;$get[uid]]]
 `
 }
