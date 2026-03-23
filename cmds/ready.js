aa7f9902d66435f4b58702b3781ea554e02b3687
6em-dev <rimit58872@gmail.com>
2026-03-23 18:03:40 +0100

    shorter time

--- cmds/ready.js
+++ cmds/ready.js
@@ -2,5 +2,5 @@ module.exports = {
     type: "clientReady",
     code: `$log[ready]
     $setStatus[dnd;Watching;$guildCount servers and $userCount members.]
-    $setInterval[$setStatus[dnd;Watching;$guildCount servers and $userCount members.];3m;status]`
+    $setInterval[$setStatus[dnd;Watching;$guildCount servers and $userCount members.];1m;status]`
 }
