ca6d60f9a6405f66779846086894bb0a2035aa4c
6em-dev <rimit58872@gmail.com>
2026-03-23 17:55:44 +0100

    i missed the status thingy

--- cmds/ready.js
+++ cmds/ready.js
@@ -1,6 +1,6 @@
 module.exports = {
     type: "clientReady",
     code: `$log[ready]
-    $setStatus[$guildCount servers;Watching;$guildCount servers and $userCount members.]
+    $setStatus[dnd;Watching;$guildCount servers and $userCount members.]
     $setInterval[$setStatus[dnd;Watching;$guildCount servers and $userCount members.];3m;status]`
 }
