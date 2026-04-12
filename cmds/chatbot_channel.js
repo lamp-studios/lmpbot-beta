8cedab45329be90d5a71ee9c8b73d3c8cd44f328
6em-dev <rimit58872@gmail.com>
2026-04-12 14:09:40 +0200

    why is there a true again!!!

--- cmds/chatbot_channel.js
+++ cmds/chatbot_channel.js
@@ -4,6 +4,6 @@ module.exports = {
     $fetchApplicationEmojis
     $let[loadingMsgID;$sendMessage[$channelID;Loading... <a:CircleLoader:1492857500637335685>;true]]
     $let[geminiReply;$callFunction[chatGemini;$messageContent]]
-    $editMessage[$channelID;$get[loadingMsgID];$get[geminiReply]]
+    $!editMessage[$channelID;$get[loadingMsgID];$get[geminiReply]]
     ;]`,
 }
