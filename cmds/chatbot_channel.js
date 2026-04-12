5a6dae49d83a80b13b3131fcaedcce933f6d8af6
6em-dev <rimit58872@gmail.com>
2026-04-12 14:03:43 +0200

    loading thingy

--- cmds/chatbot_channel.js
+++ cmds/chatbot_channel.js
@@ -1,6 +1,8 @@
 module.exports = {
     type: "messageCreate",
     code: `$!if[$channelID==$getGuildVar[chatbot_channel;$guildID];
+    $fetchApplicationEmojis
+    Loading... <a:CircleLoader:1492857500637335685>
     $callFunction[chatGemini;$messageContent]
     ;]`,
 }
