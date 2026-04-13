b403d8760a3877dbddcc3cdf755f1940e664e412
6em-dev <rimit58872@gmail.com>
2026-04-13 13:15:08 +0200

    fix: I think the api key may be a better idea

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ load_dotenv()
 class Config:
     def __init__(self):
         self.token = os.getenv("DANGER_DONTSHARETOYKEN")
+        self.gemini_api_key = os.getenv("GEMINI_API_KEY")
         self.prefix = "."
