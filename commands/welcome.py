1813172513007e31edb87431a7ccded6db9122c5
6em-dev <rimit58872@gmail.com>
2026-04-13 15:58:23 +0200

    fix

--- commands/welcome.py
+++ commands/welcome.py
@@ -1,32 +1,30 @@
 import discord
-from discord import app_commands
 
 
-def setup(tree: app_commands.CommandTree):
-    @app_commands.command(
+def setup(bot: discord.Bot):
+    @bot.slash_command(
         name="welcome",
         description="Resends all welcome messages of all members to a specified channel.",
-    )
-    @app_commands.default_permissions(administrator=True)
-    @app_commands.describe(
-        channel="The channel to send the messages in.",
-        mentions="Mentions every members in the messages if true",
-        welcome_message="The welcome message",
+        default_member_permissions=discord.Permissions(administrator=True),
     )
     async def welcome_cmd(
-        interaction: discord.Interaction,
-        channel: discord.TextChannel,
-        mentions: bool,
-        welcome_message: str | None = None,
+        ctx: discord.ApplicationContext,
+        channel: discord.TextChannel = discord.Option(
+            discord.TextChannel, "The channel to send the messages in."
+        ),
+        mentions: bool = discord.Option(
+            bool, "Mentions every members in the messages if true"
+        ),
+        welcome_message: str = discord.Option(
+            str, "The welcome message", required=False, default=None
+        ),
     ):
-        await interaction.response.send_message("Sending welcome messages...", ephemeral=True)
+        await ctx.respond("Sending welcome messages...", ephemeral=True)
 
         mention_setting = discord.AllowedMentions.all() if mentions else discord.AllowedMentions.none()
 
-        for member in interaction.guild.members:
+        for member in ctx.guild.members:
             await channel.send(
-                f"\nWelcome {member.mention} to **{interaction.guild.name}**!\n\nMake sure to read the messages!",
+                f"\nWelcome {member.mention} to **{ctx.guild.name}**!\n\nMake sure to read the messages!",
                 allowed_mentions=mention_setting,
             )
-
-    tree.add_command(welcome_cmd)
